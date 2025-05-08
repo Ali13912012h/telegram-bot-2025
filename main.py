ae40d7617eca436ad5622ff398cb7e0ac30ab2bd
Ali13912012h <toobet598@gmail.com>
2025-05-08 11:58:55 +0330

    Update mayin.py
    return

--- main.py
+++ main.py
@@ -22,9 +22,6 @@ import jdatetime
 # تنظیم لاگ‌گذاری
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
-
-# توکن بات
-TOKEN = '8122288665:AAHVQ3pVTrfTj0a_caGuITz3K4vxXoO9vcw'
 SALT = b'my_super_secret_salt_123'
 active_nodes = set()
 node_cache = {}
